81c6a05c70318e7b252c1e4caa4e07c33097b2a5
Jake Reck <jakereck@gmail.com>
2018-08-04 16:39:18 -0700

    Changed up routing for search function

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.post('/search', (req, res) => {
                 or zip = '${searchParam}'`;
     let query = db.query(sql, (err, results) => {
         if (err) throw err;
+        console.log(results);
         res.render('therapies', {
             therapies: results
         });
