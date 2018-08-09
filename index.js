f23204d566e6cfa45240a5eb985ac83f1391a421
Jake Reck <jakereck@gmail.com>
2018-08-09 06:28:22 -0700

    Getting app ready for heroku

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const express = require('express'),
     LocalStrategy = require('passport-local'),
     app = express();
 
+var PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -72,6 +74,6 @@ app.post('/register', (req, res) => {
 });
 
 
-app.listen('3000', () => {
+app.listen(PORT, () => {
     console.log("Server started on port 3000");
 });
