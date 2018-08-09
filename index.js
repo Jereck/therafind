ecaf7a8adb80155bb39764228fdb04ee08238bf8
Jake Reck <jakereck@gmail.com>
2018-08-09 06:51:55 -0700

    Setting up for heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express'),
     LocalStrategy = require('passport-local'),
     app = express();
 
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -74,6 +74,6 @@ app.post('/register', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log("Server started on port 3000");
+app.listen(port, () => {
+    console.log("Server started on port: " + port);
 });
