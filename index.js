85a080894c4c61e117ca4c17b3c21baae29a81f5
Jake Reck <jakereck@gmail.com>
2018-08-10 19:13:27 -0700

    Added login/logout logic

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const   express         = require('express'),
         bodyParser      = require('body-parser'),
         mongoose        = require('mongoose'),
         passport        = require('passport'),
-        flash           = require('connect-flash'),
-        crypto          = require('crypto'),
-        sess            = require('express-session'),
         LocalStrategy   = require('passport-local'),
 
         Therapy         = require('./models/therapy'),
@@ -17,16 +14,31 @@ var port = process.env.PORT || 3000;
 // MONGOOSE
 mongoose.connect('mongodb://Jereck:stella1011@ds119422.mlab.com:19422/therafind')
 
+// BODY PARSER SETUP
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// STATIC FILES
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
 
+
+// VIEW ENGINE
 app.set("view engine", "ejs");
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Stella is the best dog ever!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -72,8 +84,8 @@ app.get('/therapy/:id', (req, res) => {
         } else {
             res.render("show", {therapy: foundTherapy});
         }
-    })
-})
+    });
+});
 
 app.get('/register', (req, res) => {
     res.render("register");
@@ -83,6 +95,17 @@ app.post('/register', (req, res) => {
     let therapy = req.body.therapy;
     let user = req.body.user;
 
+    let newUser = new User({ username: req.body.user.email });
+    User.register(newUser, req.body.user.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render("/register");
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/");
+        });
+    });
+
     Therapy.create(therapy, user, (err, newlyCreated) => {
         if (err) {
             console.log(err);
@@ -91,6 +114,22 @@ app.post('/register', (req, res) => {
         }
     });
 });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate("local", 
+    {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    }), (req, res) => {
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+})
 
 app.listen(port, () => {
     console.log("Server started on port: " + port);
